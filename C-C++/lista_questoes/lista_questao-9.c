7cade3829a978e3d19cdd18948d411c7185ca36a
Vitórya Medeiros <graziellevitorya05@gmail.com>
2024-04-03 17:56:42 -0300

    Add files via upload

---
+++ C-C++/lista_questoes/lista_questao-9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	
+	printf("Digite um valor para A: ");
+	scanf("%i", &a);
+	
+	printf("Digite um valor para B: ");
+	scanf("%i", &b);
+	
+	printf("o valor de A eh %d e o valor de B eh %d", b,a);
+	
+	return 0;
+}
