ed78a756c2fdc6f9c046687f9ddcd55ef3c096e8
vitorya <graziellevitorya05@gmail.com>
2024-04-06 16:05:42 -0300

    up

--- C-C++/lista_questoes/comandos-condicionais/lista_questao-20.c
+++ C-C++/lista_questoes/comandos-condicionais/lista_questao-20.c
@@ -4,23 +4,31 @@ int main(){
 	
 	int n1, n2, n3;
 	
-	printf("Digite um numero: ");
-	scanf("%i", &n1);
+	printf("Digite tres numeros: ");
+	scanf("%i %i %i", &n1, &n2, &n3);	
 	
-	printf("Digite outro numero: ");
-	scanf("%i", &n2);
-
-	printf("Digite outro numero: ");
-	scanf("%i", &n3);
 	
-	if (n1 > n2 && n2 > n3){
-		printf("A ordem crescente dos numeros digitado eh %d, %d e %d", n1, n2,n3 );
+	if (n1 <= n2 && n2 <= n3){
+		printf("A ordem crescente: %d, %d, %d", n1, n2,n3 );
 	}
-	else if (n2 > n1 && n1 > n3){
-		printf("A ordem crescente dos numeros digitado eh %d, %d e %d", n3, n1, n2);
+	else if (n1 <= n3 && n3 <= n2){
+	   	printf("A ordem crescente: %d, %d, %d", n1, n3, n2);
+	}
+	
+	
+	else if (n2 <= n1 && n1 <= n3){
+	   	printf("A ordem crescente: %d, %d, %d", n2, n1, n3);
+	}
+	else if (n2 <= n3 && n3 <= n1){
+		printf("A ordem crescente: %d, %d, %d", n2, n3, n1);
+	}
+	
+	
+	else if (n3 <= n1 && n1 <= n2){
+	   	printf("A ordem crescente: %d, %d, %d", n3, n1, n2);
 	}
 	else{
-		printf("A ordem crescente dos numeros digitado eh %d, %d e %d", n3, n2, n1);
+		printf("A ordem crescente: %d, %d, %d", n3, n2, n1);
 	}
 	
 	return 0;
