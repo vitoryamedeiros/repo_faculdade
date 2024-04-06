819a569e4ca20d04c8b58e8b75cb726d8e1faea8
vitorya <graziellevitorya05@gmail.com>
2024-04-06 17:01:08 -0300

    Update lista_questao-20.c

--- C-C++/lista_questoes/comandos-condicionais/lista_questao-20.c
+++ C-C++/lista_questoes/comandos-condicionais/lista_questao-20.c
@@ -7,7 +7,6 @@ int main(){
 	printf("Digite tres numeros: ");
 	scanf("%i %i %i", &n1, &n2, &n3);	
 	
-	
 	if (n1 <= n2 && n2 <= n3){
 		printf("A ordem crescente: %d, %d, %d", n1, n2,n3 );
 	}
@@ -15,14 +14,12 @@ int main(){
 	   	printf("A ordem crescente: %d, %d, %d", n1, n3, n2);
 	}
 	
-	
 	else if (n2 <= n1 && n1 <= n3){
 	   	printf("A ordem crescente: %d, %d, %d", n2, n1, n3);
 	}
 	else if (n2 <= n3 && n3 <= n1){
 		printf("A ordem crescente: %d, %d, %d", n2, n3, n1);
 	}
-	
 	
 	else if (n3 <= n1 && n1 <= n2){
 	   	printf("A ordem crescente: %d, %d, %d", n3, n1, n2);
